13855800757f8c6d3806fa7264c13fc3c82d32a9
Justin Tungul <justin.tungul@gmail.com>
2018-06-11 14:29:22 -0700

    Generate all controllers for public and admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,35 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+  end
+  namespace :admin do
+    get 'users/new'
+    get 'users/create'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/destroy'
+    get 'users/index'
+    get 'users/show'
+  end
+  namespace :admin do
+    get 'comments/destroy'
+  end
+  get 'contacts/new'
+  get 'contacts/create'
+  get 'about/index'
+  get 'comments/new'
+  namespace :admin do
+    get 'categories/new'
+    get 'categories/create'
+    get 'categories/edit'
+    get 'categories/update'
+    get 'categories/destroy'
+    get 'categories/index'
+    get 'categories/show'
+  end
+  get 'categories/show'
+  namespace :admin do
     get 'posts/new'
     get 'posts/create'
     get 'posts/edit'
